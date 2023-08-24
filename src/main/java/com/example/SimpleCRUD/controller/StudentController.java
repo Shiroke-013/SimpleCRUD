8aa5219cafba35e048b61d3e7c92c7a45bf5d387
Andres Chaves <andreschaves@Andress-MacBook-Air.local>
2023-08-24 17:11:29 -0500

    Importing used libraries

--- src/main/java/com/example/SimpleCRUD/controller/StudentController.java
+++ src/main/java/com/example/SimpleCRUD/controller/StudentController.java
@@ -3,8 +3,13 @@ package com.example.SimpleCRUD.controller;
 import com.example.SimpleCRUD.model.Student;
 import com.example.SimpleCRUD.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
